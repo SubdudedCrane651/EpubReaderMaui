de5cab1d3d50ec0a27e052497cb037c114d05dce
Richard <rchrdperreault@gmail.com>
2026-02-23 05:47:17 -0500

    tryed with epub.min.js no success

---
+++ EpubReaderMaui/LocalHttpServer.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+
+namespace EpubReaderMaui;
+
+public class LocalHttpServer
+{
+    private readonly HttpListener _listener;
+    private readonly string _filePath;
+
+    public string Url { get; }
+
+    public LocalHttpServer(string filePath, int port = 12345)
+    {
+        _filePath = filePath;
+        Url = $"http://localhost:{port}/book.epub";
+
+        _listener = new HttpListener();
+        _listener.Prefixes.Add($"http://*:{port}/");
+    }
+
+    public void Start()
+    {
+        _listener.Start();
+        Task.Run(async () =>
+        {
+            while (_listener.IsListening)
+            {
+                HttpListenerContext ctx;
+                try
+                {
+                    ctx = await _listener.GetContextAsync();
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (ctx.Request.RawUrl == "/book.epub")
+                {
+                    try
+                    {
+                        byte[] bytes = File.ReadAllBytes(_filePath);
+                        ctx.Response.ContentType = "application/epub+zip";
+                        ctx.Response.ContentLength64 = bytes.Length;
+                        await ctx.Response.OutputStream.WriteAsync(bytes, 0, bytes.Length);
+                        ctx.Response.OutputStream.Close();
+                    }
+                    catch
+                    {
+                        ctx.Response.StatusCode = 500;
+                        ctx.Response.Close();
+                    }
+                }
+                else
+                {
+                    ctx.Response.StatusCode = 404;
+                    ctx.Response.Close();
+                }
+            }
+        });
+    }
+
+    public void Stop()
+    {
+        try
+        {
+            if (_listener.IsListening)
+                _listener.Stop();
+        }
+        catch { }
+    }
+}
---
+++ EpubReaderMaui/Resources/Raw/reader/reader.js
@@ -0,0 +1,32 @@
+﻿var book = null;
+window.rendition = null;
+
+alert("reader.js loaded");
+
+window.loadTestBook = function () {
+    alert("loadTestBook called");
+
+    // Public test EPUB (Moby Dick)
+    book = ePub("https://s3.amazonaws.com/moby-dick/OPS/package.opf");
+
+    window.rendition = book.renderTo("viewer", {
+        width: "100%",
+        height: "100%",
+        spread: "none"
+    });
+
+    window.rendition.display();
+
+    // Send chapter list to MAUI
+    book.loaded.navigation.then(function (nav) {
+        var chapters = nav.toc.map(function (item) {
+            return {
+                Title: item.label,
+                Href: item.href
+            };
+        });
+
+        var json = encodeURIComponent(JSON.stringify(chapters));
+        window.location.href = "chapters://" + json;
+    });
+};
